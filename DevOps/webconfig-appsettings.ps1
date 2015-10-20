279856c791ac2c9d4c96ed4c6ab3e0b6743f9e09
Kevin Gjerstad <kevingjerstad@live.com>
2015-10-20 14:24:21 -0700

    webconfig script

--- DevOps/webconfig-appsettings.ps1
+++ DevOps/webconfig-appsettings.ps1
@@ -1,7 +1,11 @@
-﻿$webConfig = '**\Tailspin.WebUpgraded\Web.config'
+﻿param(
+[string]$tagstring
+)
+
+$webConfig = '**\Tailspin.WebUpgraded\Web.config'
 $doc = (Get-Content $webConfig) -as [Xml]
 $obj = $doc.configuration.appSettings.add | where {$_.Key -eq 'iKey'}
 $obj.value = 'bb2f698c-c81a-4224-9d13-39fbf4f39452'
 $obj = $doc.configuration.appSettings.add | where {$_.Key -eq 'tags'}
-$obj.value = 'productionA'
+$obj.value = $tagstring
 $doc.Save($webconfig)
